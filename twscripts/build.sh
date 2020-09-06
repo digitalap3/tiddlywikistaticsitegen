16f48a208d87e8632c42953402663f446468c3f2
digitalap3 <digitalap3@gmail.com>
2020-09-06 16:56:15 -0400

    Delete build.sh

--- twscripts/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-tiddlywiki ./myblog --render "[page-type[post]publish[yes]]" "[has[created]addsuffix[.html]]" "text/plain" "$:/blog/templates/static.tiddler.html"
-
-tiddlywiki ./myblog --render "[page-type[index]]" "[has[created]addsuffix[.html]]" "text/plain" "$:/blog/templates/static.tiddler.html"
-
-tiddlywiki ./myblog --render "[[static.css]]" "[is[tiddler]]" "text/plain"
