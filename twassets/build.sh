98ee2d09fb38712497737ed01fd5e368dfd06426
digitalap3 <digitalap3@gmail.com>
2020-09-06 23:17:11 -0400

    Update build.sh

--- twassets/build.sh
+++ twassets/build.sh
@@ -5,6 +5,10 @@ tiddlywiki ./myblog --render "[page-type[index]]" "[has[created]addsuffix[.html]
 
 tiddlywiki ./myblog --render "[[static.css]]" "[is[tiddler]]" "text/plain"
 
+tiddlywiki --save [is[image]tag[header-img]] [addprefix[./images/headers/]]
+
+tiddlywiki --setfield [is[image]tag[header-img]] _canonical_uri $:/blog-headers/templates/canonical-uri-external-image text/plain
+
 tiddlywiki --save [is[image]] [addprefix[./images/$myyear/$mymonth/]]
 
 tiddlywiki --setfield [is[image]] _canonical_uri $:/blog/templates/canonical-uri-external-image text/plain
