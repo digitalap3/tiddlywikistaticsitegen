4828e185b54efee491de20c1b2f2653d53c539ee
digitalap3 <digitalap3@gmail.com>
2020-09-10 16:24:17 -0700

    change in folder structure

--- twassets/httpserverup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd ./myblog/output
-nohup http-server -H 0.0.0.0 -p 8081 > ../httpserver.out &
--- twassets/twstaticup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-nohup tiddlywiki ./myblog --listen host=0.0.0.0 username=username password=password "readers=(anon)" > twserverup.out &
